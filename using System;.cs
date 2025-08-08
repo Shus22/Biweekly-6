a00bcc2551d8accb0a37d7615aa5b41595fa4b56
Shus22 <st704608@gmail.com>
2025-08-08 20:12:58 +1000

    Create using System;.cs
    Added DisplayInfo method and summary comment

---
+++ using System;.cs
@@ -0,0 +1,25 @@
+
+using System;
+
+// This class stores customer details like ID, name, and email.
+class Customer
+{
+    // Properties of the Customer class
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    // Constructor to initialize the properties
+    public Customer(int id, string name, string email)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+    }
+
+    // Method to display customer information
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Customer ID: {Id}, Name: {Name}, Email: {Email}");
+    }
+}
